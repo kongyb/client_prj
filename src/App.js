edde4331f79c4cbf75aaec9d8f28f9dd523f8a7e
kongyb <kongyb08@naver.com>
2021-12-20 17:20:58 +0900

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+  const [data,setData]=useState('');
+  const [insertMode,setInsertMode]=useState(false);
+  const [serverData,setServerData]=useState('');
+  const textInput=(e)=>{
+    setData(e.target.value);
+  }
+
+  const checkData=()=>{
+    console.log(data);
+  }
+
+  const request=()=>{
+    Axios({
+      method:'post',
+      url:'http://localhost:9999',
+      data:{data:data}
+    }).then((res)=>{
+      console.log(res.data);
+      setServerData(res.data);
+    })
+  }
+
+  const insert=()=>{
+    setInsertMode(!insertMode);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea onChange={textInput}></textarea><br/>
+      <button onClick={checkData}>내용확인</button><br/>
+      <button onClick={request}>서버전송</button>
+      <br/>
+      <br/>
+      <pre>{data}</pre>
+      <br/>
+      <br/>
+      <div>
+        {insertMode?<textarea>{serverData}</textarea>:<pre>{serverData}</pre>}
+      </div>
+      <button onClick={insert}>수정모드</button>
     </div>
   );
 }
